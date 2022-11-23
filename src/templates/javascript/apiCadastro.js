20814b071e1200820c20dc1e8192eac7d892caf0
Pablo Guilherme <pablo.gapm@gmail.com>
2022-11-23 15:01:59 -0300

    criacao da integracao do cadastro

---
+++ src/templates/javascript/apiCadastro.js
@@ -0,0 +1,31 @@
+function doPost(url, body){
+
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = function(){
+        console.log(this.responseText)
+    }
+
+    return request.responseText
+}
+
+function cadastroUsuario(){
+
+    let url = "http://localhost:5000/api/autenticacao/cadastro" 
+    let nome = document.getElementById("nome").value
+    let telefone = document.getElementById("telefone").value
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+
+    body = {
+        "nome": nome,
+        "telefone": telefone,
+        "email": email,
+        "senha": senha
+    }
+
+    doPost(url, body)
+}
