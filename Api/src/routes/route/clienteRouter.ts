d9ebe76cbade346576db1bd5860471380d96f554
Eduardo Brito <83357278+EduardoABrito@users.noreply.github.com>
2022-11-23 15:12:42 -0300

    criar rota para puxar unico usuario#99

--- Api/src/routes/route/clienteRouter.ts
+++ Api/src/routes/route/clienteRouter.ts
@@ -9,7 +9,14 @@ export const clienteRouter = Router()
 
 clienteRouter.get("/",async (req, res, next) => {
     // #swagger.tags = ['Cliente']
-   await DBEntities.Cliente.findAll({
+   await DBEntities.Cliente.findAll()
+   .then((result)=>{ res.status(200).json(result) })
+   .catch((e: any) => next(new HttpException(500, "Erro inesperado tente Novamente mais tarde")))
+})
+clienteRouter.get("/:cliente_id",async (req, res, next) => {
+    // #swagger.tags = ['Cliente']
+   await DBEntities.Cliente.findOne({
+    where: {'id': req.params.cliente_id},
     include: {model: DBEntities.Endereco}
    })
    .then((result)=>{ res.status(200).json(result) })
