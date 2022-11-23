9f771d9bcdf4ed553be09ec3bebc72137ce3832a
Pablo Guilherme <pablo.gapm@gmail.com>
2022-11-23 15:11:46 -0300

    integracao com a api de cadastro referente ao issue #98

--- src/templates/javascript/apiCadastro.js
+++ src/templates/javascript/apiCadastro.js
@@ -4,12 +4,9 @@ function doPost(url, body){
     request.open("POST", url, true)
     request.setRequestHeader("Content-type", "application/json")
     request.send(JSON.stringify(body))
-
-    request.onload = function(){
-        console.log(this.responseText)
-    }
-
+    
     return request.responseText
+    
 }
 
 function cadastroUsuario(){
