0e9da601f65ade0895dd058dd538609c6b06b4fa
Eduardo Brito <83357278+EduardoABrito@users.noreply.github.com>
2022-11-29 12:38:08 -0300

    integrar estoque #104

---
+++ src/templates/javascript/apiEstoque.js
@@ -0,0 +1,50 @@
+let estoque = {
+    variaveis: {
+        url: "http://localhost:5000/api/ingrediente/"
+    },
+    funcoes:{
+        montarTable: () => {
+            swal.fire({
+                icon: "info",
+                title: "Aguarde",
+                text: "Carregando estoque",
+                timer: 4000, 
+                showConfirmButton: false,
+                timerProgressBar: true,
+                toast: true,
+                position: "top-right"                    
+            })
+            $('#estoque-table').DataTable({
+                "paging": false,
+                "lengthChange": true,
+                "info": false,
+                "autoWidth": true,
+                "destroy": true,
+                "ajax": {                   
+                    "url": estoque.variaveis.url,
+                    "type": 'GET',
+                    "dataSrc": (value) =>{
+                        return value;
+                    },
+                },
+                "columns": [
+                    {"data": "id"},
+                    {"data": "nome"},
+                    {"data": "quantidade", "render": (data, row) => {
+                        return data > 0 ? data : "esgotado";
+                    }},
+                    {"data": "valor_unidade", "render": (data, row)=>{
+                        console.table(row)
+                        return `R$: ${data.toLocaleString('pt-br', {minimumFractionDigits: 2})}`;
+                    }}
+                ]
+            })                 
+        },
+        init: ()=>{
+            estoque.funcoes.montarTable();
+        }
+    }
+}
+$(document).ready(e=>{
+    estoque.funcoes.init();
+})
