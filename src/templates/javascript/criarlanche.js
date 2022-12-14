26a1bef1cdd982f7e6cf388199ae67f9bc8f0015
guicostak <103201399+guicostak@users.noreply.github.com>
2022-12-14 00:37:14 -0300

    Criando validação

--- src/templates/javascript/criarlanche.js
+++ src/templates/javascript/criarlanche.js
@@ -1,4 +1,9 @@
+
+var nome = document.querySelector("#nome")
 var preco = document.querySelector("#preco")
+var labelErro = document.querySelector("#labelErro")
+
+
 
 preco.addEventListener('keypress', () =>{
   if (preco.value.length === 0) {
@@ -9,3 +14,13 @@ preco.addEventListener('keypress', () =>{
 preco.addEventListener('focusout', () =>{
   preco.value +=',00'
 })
+
+function valida() {
+  if(nome.value.length === 0 || preco.value.length === 0){
+    Swal.fire({
+      icon: 'error',
+      title: 'Erro de cadastro',
+      text: 'Preencha todos os campos do formulário!',
+    })
+  }
+}
