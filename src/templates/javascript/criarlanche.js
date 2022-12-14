2ea086e56997fe716a2835477177576f9d521c3b
henrique <henriquebd2000.hs@gmail.com>
2022-12-14 14:50:22 -0300

    adicionando funcionalidade #124

--- src/templates/javascript/criarlanche.js
+++ src/templates/javascript/criarlanche.js
@@ -2,7 +2,11 @@
 var nome = document.querySelector("#nome")
 var preco = document.querySelector("#preco")
 var descricao = document.querySelector("#descricao")
-
+const select_ingrediente = document.getElementById('ingrediente')
+const adicionar = document.querySelector('#adicionar')
+const ingredientes_adicionados = document.getElementById('ingredientes-adicionados')
+let adicionado
+let qtd_ingredientes = 0
 
 preco.addEventListener('keypress', () =>{
   if (preco.value.length === 0) {
@@ -24,5 +28,47 @@ function valida() {
       title: 'Erro de cadastro',
       text: 'Preencha todos os campos do formulário!',
     })
+  }
+}
+
+ingrediente.addEventListener('change', adicionar_ingredientes)
+function adicionar_ingredientes(){
+  let selecionado = select_ingrediente.options[select_ingrediente.selectedIndex]
+  let texto = selecionado.text
+  return texto
+}
+
+adicionar.addEventListener('click', () => {
+  adicionado = adicionar_ingredientes()
+  let selecionado = select_ingrediente.options[select_ingrediente.selectedIndex]
+
+  if(selecionado.value != 0){
+    let novo_ingrediente = document.createElement('div')
+    novo_ingrediente.className = 'ingrediente-adicionado'
+  
+    ingredientes_adicionados.classList.remove('display-none')
+    ingredientes_adicionados.append(novo_ingrediente)
+    novo_ingrediente.innerHTML = `<p class="ingrediente">${adicionado}</p> 
+    <button id="excluir" onclick="excluir_ingrediente()" ><i class="fa-solid fa-trash trash-icon"></i></button>`
+    qtd_ingredientes++
+
+  } else {
+
+    const ingrediente_nao_selecionado = document.getElementById('nao-selecionado')
+    ingrediente_nao_selecionado.classList.remove('display-none')
+
+    setTimeout(() => {
+      ingrediente_nao_selecionado.classList.add('display-none')
+    }, 2000)
+  }
+})
+
+function excluir_ingrediente() {
+  const ingrediente_adicionado = document.querySelector('.ingrediente-adicionado')
+  ingrediente_adicionado.remove()
+  qtd_ingredientes--
+  
+  if(qtd_ingredientes <= 0) {
+    ingredientes_adicionados.classList.add('display-none')
   }
 }
